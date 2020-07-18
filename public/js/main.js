85de9b3ddd112c479da96c13027dd63912bf08b3
Domini3 <dome_jannak@hotmail.com>
2020-07-18 17:10:59 +0700

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+
+socket.emit('joinRoom', { username});
+
+socket.on('roomUsers', ({ users }) => {
+  outputUsers(users);
+});
+
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+
+  socket.emit('chatMessage', msg);
+
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
+}
+$.ajax({
+  url : "http://localhost:3000" + "/get_messages" ,
+  method : "GET" ,
+  success : function(response) {
+    console.log(response) ;
+
+    var data =JSON.parse(response) ;
+    for (var a = 0 ; a <data.length;a++){
+      const div = document.createElement('div');
+    
+      div.innerHTML = `<p class="meta">${data[a].Name}</p>
+      <p class="text">${data[a].message}</p>`;
+      
+      div.classList.add('message');
+
+      document.querySelector('.chat-messages').appendChild(div);
+    }
+  }
+});
